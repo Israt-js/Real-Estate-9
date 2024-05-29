855bde6921b6c974f902bace1546dfabc2783e1f
israt.js <israt8291jahan@gmail.com>
2024-05-29 16:21:28 +0600

    first commit

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -93,6 +93,7 @@ const Home = () => {
       <h1 className="text-4xl font-extrabold mt-5 text-center">Special Home Available</h1>
       <SpecialHome/>
       </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7498126.16960131!2d85.05446553552625!3d23.41734307029925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30adaaed80e18ba7%3A0xf2d28e0c4e1fc6b!2sBangladesh!5e0!3m2!1sen!2sbd!4v1714189307818!5m2!1sen!2sbd" width="800" height="600" className="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   )
 }
